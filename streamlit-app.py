5913646cf8af1db90308b2c1747827758f1ee85c
premstaller1 <premstaller-spam@outlook.de>
2024-05-31 00:54:26 +0200

    app is running, adjustments

--- streamlit-app.py
+++ streamlit-app.py
@@ -9,18 +9,13 @@ model_name = "ElKulako/cryptobert"
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
 model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=3)
 pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer, max_length=64, truncation=True, padding='max_length')
-
-st.header('Sentiment Analysis')
 with st.expander('Analyze Text'):
     text = st.text_input('Text here: ')
     if text:
-        # Convert to list
-        posts = text.tolist()
         explainer = shap.Explainer(pipe)
-        shap_values = explainer(posts)
+        shap_values = explainer([text])  # Pass text directly as a list
         shap_values
         shap.plots.text(shap_values[:, :, "Bullish"])
-        # Plot the SHAP bar plot
         shap.plots.bar(shap_values[:, :, "Bearish"].mean(0))
 
 
